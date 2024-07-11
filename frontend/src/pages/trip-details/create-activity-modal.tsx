28d0f0eb3777608d9f9e54b7b2238827e17e6582
Cayque Silva <cayque.ti@gmail.com>
2024-07-11 17:23:11 -0300

    chore: cadastro de atividades, retornando id de atividades e apresentando em tela de resumo.

--- frontend/src/pages/trip-details/create-activity-modal.tsx
+++ frontend/src/pages/trip-details/create-activity-modal.tsx
@@ -1,12 +1,35 @@
 import { Calendar, Plus, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
 
 interface CreateActivityModalProps{
     closeCreateActivityModal: () => void,
-
 }
 
 export function CreateActivityModal({closeCreateActivityModal}: CreateActivityModalProps){
+
+    const {tripId} = useParams();
+
+    function createActivity(event: FormEvent<HTMLFormElement>){
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget);
+
+        const title = data.get('title')?.toString();
+        const occurs_at = data.get('occurs_at')?.toString();
+
+        api.post(`trips/${tripId}/activities`,{
+            title,
+            occurs_at,
+        })
+
+        //closeCreateActivityModal();
+        //para dar refresh foi preciso essa gambiarra
+        window.document.location.reload();
+    }
+
     return(
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
             <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -22,7 +45,7 @@ export function CreateActivityModal({closeCreateActivityModal}: CreateActivityMo
                     </p>
                 </div>
                 
-                <form className="space-y-3">
+                <form onSubmit={createActivity} className="space-y-3">
                     <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
                         <Tag className="size-5 text-zinc-400"/>
                         <input 
